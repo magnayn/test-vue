7c44b9f750fc356dd3174a728a815a0a48dfa33c
Nigel Magnay <nigelmagnay@swg.com>
2022-02-10 17:40:16 +0000

    Asset viewer fixes for componentisation.
    Signed-off-by: Nigel Magnay <nigelmagnay@swg.com>

---
+++ asset_viewer/dev/serve.vue
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { WooyaySample } from '@/entry.esm';
+import { AssetViewer} from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+   components: {
+    AssetViewer
+   }
+});
+</script>
+
+<template>
+  <div id="app">
+
+     <asset-viewer xktId="534"></asset-viewer>
+
+  </div>
+</template>
---
+++ asset_viewer/src/lib-components/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+export { default as WooyaySample } from './wooyay-sample.vue';
+export { default as AssetViewer } from './AssetViewer.vue';
+export { default as AssetView } from './AssetView.vue';
+export { default as AssetViewToolboar } from './AssetViewToolbar.vue';
