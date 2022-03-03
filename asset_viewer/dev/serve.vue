45ef979ea28701e8cdb39c156523ce0af0065807
magnayn <nigel.magnay@nirima.com>
2022-03-03 14:01:59 +0000

    serve test

--- asset_viewer/dev/serve.vue
+++ asset_viewer/dev/serve.vue
@@ -3,7 +3,7 @@ import { defineComponent } from 'vue';
 // Uncomment import and local "components" registration if library is not registered globally.
 // import { WooyaySample } from '@/entry.esm';
 import { AssetViewer, AssetViewToolbar} from '@/entry.esm';
-import AssetDBClient from '../src/lib-components/AssetDBConnector';
+import AssetDBClient from '../src/lib-components/AssetDBClient';
 
 // 5e25c97d-3678-463a-b105-287a31af763f&models=
 // bd19fc4c-b086-47db-beea-8ff13ccc9f00&models=
@@ -12,7 +12,7 @@ import AssetDBClient from '../src/lib-components/AssetDBConnector';
 const client = new AssetDBClient( {
             baseUrl: "https://apim-assetdb-dev.azure-api.net/asset",
             tenantId: "F3054CC9-77E5-4EAC-A310-39CAC4ACAF3E",
-            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE2NDYxNDk1MTUsIm5iZiI6MTY0NjE0NTkxNSwidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9hc3NldGRiLmIyY2xvZ2luLmNvbS9mMzA1NGNjOS03N2U1LTRlYWMtYTMxMC0zOWNhYzRhY2FmM2UvdjIuMC8iLCJzdWIiOiI2NTI4YTI1My0wZTc5LTRlODAtYjliMy05Y2MxZmZkZjNhNTEiLCJhdWQiOiIwZWVjNmI3YS0wNDJiLTQ4MzctYmVkNy03NWZjOTY2ZTBjOGMiLCJub25jZSI6IjE0OGFlOWMwLTZlYzMtNGE3Yi1iNWViLTU1MWFiNGEzNzI5ZSIsImlhdCI6MTY0NjE0NTkxNSwiYXV0aF90aW1lIjoxNjQ2MTM1NDE2LCJjb3VudHJ5IjoiVW5pdGVkIEtpbmdkb20iLCJuYW1lIjoiTmlnZWxNIiwicG9zdGFsQ29kZSI6Im94MyA5YnkiLCJlbWFpbHMiOlsibmlnZWxtYWduYXlAc3dnLmNvbSJdLCJ0ZnAiOiJCMkNfMV9TUzEifQ.UVXd0yI1OD31Zz33X-pcXx3kV-9x-mQ0atZ4rDw-svodO9vTevqC6JknIbYu_A63SzS0HpW3i3VVxgEm4QOatx1UglnM7_-iihC7MVyTc72PGiyp-PesuScNlJ9gSpXEhyuoj7icCQxbgwusDLI1EgCcO7jIt8CB5mSngUd_oMak8RWPuC8tO1RWA5ZBIxSkcWf74GK-RFlJq7Cgz4mdfJYvEo-DtuYGW8rhMs-z59c5rsKt_X7Nk_EWvBljtfB2fdQHIs4ZaZBBnplSCOmcal6nu-1VxcYJBd5Wn1QXo1Mvn7vErK_LO7nANWI7ZkOuyhsH4XbJP2NESmR2b9lNsQ"
+            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6Ilg1ZVhrNHh5b2pORnVtMWtsMll0djhkbE5QNC1jNTdkTzZRR1RWQndhTmsifQ.eyJleHAiOjE2NDYyMzc0NzYsIm5iZiI6MTY0NjIzMzg3NiwidmVyIjoiMS4wIiwiaXNzIjoiaHR0cHM6Ly9hc3NldGRiLmIyY2xvZ2luLmNvbS9mMzA1NGNjOS03N2U1LTRlYWMtYTMxMC0zOWNhYzRhY2FmM2UvdjIuMC8iLCJzdWIiOiI2NTI4YTI1My0wZTc5LTRlODAtYjliMy05Y2MxZmZkZjNhNTEiLCJhdWQiOiIwZWVjNmI3YS0wNDJiLTQ4MzctYmVkNy03NWZjOTY2ZTBjOGMiLCJub25jZSI6IjUyMTIyNGFkLWFlY2UtNDA2Zi05MzhlLTM0NWY4NmZhNTQ5MSIsImlhdCI6MTY0NjIzMzg3NiwiYXV0aF90aW1lIjoxNjQ2MjMzODc2LCJjb3VudHJ5IjoiVW5pdGVkIEtpbmdkb20iLCJuYW1lIjoiTmlnZWxNIiwicG9zdGFsQ29kZSI6Im94MyA5YnkiLCJlbWFpbHMiOlsibmlnZWxtYWduYXlAc3dnLmNvbSJdLCJ0ZnAiOiJCMkNfMV9TUzEifQ.s2ff3c8Xud6RaAkLXjgtIlSjAREWAAFhD7-pyqXN3N-LkifO6OoFg2RWyfPw4d5onAHd3AX_X8HgltzEpxjw-rqyBQ7J4GR6LKk0LMz-nJZJKPV-rSbDgbGLaJzCG6bFLPiSgy9y6d2wTN2qKqODSYYIvTkT_eEh6M301xYSEDG2u4fy6DqmjjFF11cv26WSskpsm_g7O8MZYiJBfw0ejstIe7lIuix3rjqXlEReFIY69U7vqom8bfRWE5uuxCAXqa6-oHM1BNK9Io4Q7Iy5fqNMlZtdDcPQ0ARUYayfT5LQs45dA25xBbhCUupnRH-Bc7f28DWJyvIrwccnP0l_Bg"
         });
 
 export default defineComponent({
@@ -22,7 +22,8 @@ export default defineComponent({
     AssetViewToolbar
    },
    data() { return {
-     bimId: "541871c3-ae3e-422e-a237-acf7f584c16e",
+     xkt: "http://localhost:8080/assets/Duplex.ifc.xkt",
+     
      client: client
    
 }; }})
@@ -33,7 +34,7 @@ export default defineComponent({
 <template>
   <div id="app">
     
-     <asset-viewer :bimId="bimId" :client="client"></asset-viewer>
+     <asset-viewer :xkt="xkt" :client="client"></asset-viewer>
 
   </div>
 </template>
